d1f0fd699f1ad92a590b1ce1e2ae0012f33a0761
Viacheslav <vm.php.dev@gmail.com>
2022-08-21 12:38:27 +0300

    change return to yield in the tests

--- tests/WinnerAlgorithmTest.php
+++ tests/WinnerAlgorithmTest.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 
+use Ocelot\Platinum\Data\ReservedPrice;
 use Ocelot\Platinum\Model\Bidder;
 use Ocelot\Platinum\Service\WinnerAlgorithm;
 use PHPUnit\Framework\TestCase;
@@ -19,57 +20,55 @@ class WinnerAlgorithmTest extends TestCase
 
     }
 
-    public function biddersProvider(): array
+    public function biddersProvider(): Generator
     {
-        return [
-            'technical_task' => [[
+        yield 'technical_task' => [[
                 $bidderA = new Bidder('Bidder-A', [110, 130]),
                 $bidderB = new Bidder('Bidder-B', null),
                 $bidderC = new Bidder('Bidder-C', [125]),
                 $bidderD = new Bidder('Bidder-D', [105, 115, 90]),
                 $bidderE = new Bidder('Bidder-E', [132, 135, 140]),
-            ], 100, ['Bidder-E', 130]],
-            'one_bidder_null_bids' => [[
+            ], ReservedPrice::getReservedPrice(), ['Bidder-E', 130]];
+        yield 'one_bidder_null_bids' => [[
                 $bidderB = new Bidder('Bidder-B', null),
-            ], 100, null],
-            'one_bidder_winning_bids' => [[
+            ], ReservedPrice::getReservedPrice(), null];
+        yield 'one_bidder_winning_bids' => [[
                 $bidderA = new Bidder('Bidder-A', [110, 130]),
-            ], 100, ['Bidder-A', 100]],
-            'one_bidder_below_reserve_price' => [[
+            ], ReservedPrice::getReservedPrice(), ['Bidder-A', 100]];
+        yield 'one_bidder_below_reserve_price' => [[
                 $bidderA = new Bidder('Bidder-A', [99]),
-            ], 100, null],
-            'two_bidders_one_doesnt_bid' => [[
+            ], ReservedPrice::getReservedPrice(), null];
+        yield 'two_bidders_one_doesnt_bid' => [[
                 $bidderA = new Bidder('Bidder-A', [99]),
                 $bidderB = new Bidder('Bidder-B', null),
-            ], 100, null],
-            'two_bidders_all_below_reserve_price' => [[
+            ], ReservedPrice::getReservedPrice(), null];
+        yield 'two_bidders_all_below_reserve_price' => [[
                 $bidderA = new Bidder('Bidder-A', [99]),
                 $bidderB = new Bidder('Bidder-C', [98]),
-            ], 100, null],
-            'two_bidders_one_above_reserve_price' => [[
+            ], ReservedPrice::getReservedPrice(), null];
+        yield 'two_bidders_one_above_reserve_price' => [[
                 $bidderA = new Bidder('Bidder-A', [101]),
                 $bidderB = new Bidder('Bidder-B', null),
-            ], 100, ['Bidder-A', 100]],
-            'two_bidders' => [[
+            ], ReservedPrice::getReservedPrice(), ['Bidder-A', 100]];
+        yield 'two_bidders' => [[
                 $bidderA = new Bidder('Bidder-A', [110]),
                 $bidderB = new Bidder('Bidder-C', [125]),
-            ], 100, ['Bidder-C', 110]],
-            'three_bidders' => [[
+            ], ReservedPrice::getReservedPrice(), ['Bidder-C', 110]];
+        yield 'three_bidders' => [[
                 $bidderA = new Bidder('Bidder-A', [110, 130]),
                 $bidderB = new Bidder('Bidder-B', null),
                 $bidderB = new Bidder('Bidder-C', [125]),
-            ], 100, ['Bidder-A', 125]],
-            'three_bidders_one_null_one_below_reserve_price' => [[
+            ], ReservedPrice::getReservedPrice(), ['Bidder-A', 125]];
+        yield 'three_bidders_one_null_one_below_reserve_price' => [[
                 $bidderA = new Bidder('Bidder-A', [110, 130]),
                 $bidderB = new Bidder('Bidder-B', null),
                 $bidderB = new Bidder('Bidder-C', [90]),
-            ], 100, ['Bidder-A', 100]],
-            'four and more_bidders' => [[
+            ], ReservedPrice::getReservedPrice(), ['Bidder-A', 100]];
+        yield 'four and more_bidders' => [[
                 $bidderA = new Bidder('Bidder-A', [110, 130]),
                 $bidderB = new Bidder('Bidder-B', null),
                 $bidderC = new Bidder('Bidder-C', [125]),
                 $bidderD = new Bidder('Bidder-D', [105, 115, 90]),
-            ], 100, ['Bidder-A', 125]],
-        ];
+            ], ReservedPrice::getReservedPrice(), ['Bidder-A', 125]];
     }
 }
